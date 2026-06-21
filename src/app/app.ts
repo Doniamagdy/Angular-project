7e2e9ab1bec5837ad3d1e607569bb5072bee835b
Doniamagdy <donallino@gmail.com>
2026-06-21 21:20:11 +0300

    Initial Angular project setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home , About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
